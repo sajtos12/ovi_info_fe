3af799ebe6e1d47880f9b30877f41d25b9bc01a0
sajtos12 <bazso.k12@gmail.com>
2020-02-23 22:21:30 +0100

    basic login page layout with form validaton

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import { Redirect, BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/login/login";
+import Home from "./components/home/home";
 
 function App() {
   return (
     <div>
       <Router>
         <Redirect to="/login" />
-        <Route path="/login" component={Login} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/home" component={Home} />
       </Router>
     </div>
   );
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,5 +1,82 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Redirect } from "react-router";
 
 export default function Login() {
-  return <div>Login page</div>;
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+  const [valid, setValid] = useState(true);
+  const [success, setSuccess] = useState(false);
+
+  const onChange = () => {
+    if (usernameRef.current.value !== "" && passwordRef.current.value !== "") {
+      setValid(false);
+    } else {
+      setValid(true);
+    }
+  };
+
+  const login = event => {
+    event.preventDefault();
+    console.log(usernameRef.current.value);
+    console.log("login");
+    setSuccess(true);
+  };
+
+  return (
+    <React.Fragment>
+      <div className="hero is-medium is-desktop">
+        <div className="hero-body">
+          <div className="columns is-centered">
+            <div className="column is-one-quarter">
+              <h1 className="title">Bejelentkezés</h1>
+              <div className="container is-fullhd">
+                <form onSubmit={login}>
+                  <div className="field">
+                    <div className="control has-icons-left">
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="Felhasználónév"
+                        ref={usernameRef}
+                        onChange={onChange}
+                      />
+                      <span className="icon is-small is-left">
+                        <i className="fas fa-user"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div className="control has-icons-left">
+                      <input
+                        className="input"
+                        type="password"
+                        placeholder="Jelszó"
+                        ref={passwordRef}
+                        onChange={onChange}
+                      />
+                      <span className="icon is-small is-left">
+                        <i className="fas fa-lock"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div className="control">
+                      <button
+                        className="button is-primary"
+                        disabled={valid}
+                        type="submit"
+                      >
+                        Bejelentkezés
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {success && <Redirect to="/home" />}
+    </React.Fragment>
+  );
 }
