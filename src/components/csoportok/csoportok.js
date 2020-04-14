f1bd88509cc9b96e490fa8ae265ecd0b27edab17
sajtos12 <bazso.k12@gmail.com>
2020-04-14 17:16:03 +0200

    added csoportok page

---
+++ src/components/csoportok/csoportok.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Csoportok = () => {
+  return <div>Csoportok</div>;
+};
+export default Csoportok;
--- src/components/skeleton/body.js
+++ src/components/skeleton/body.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch } from 'react-router';
 import ProtectedRoute from '../protected/protected-route';
 import Home from '../home/home';
+import Csoportok from '../csoportok/csoportok';
 
 export const Body = () => {
   return (
     <div>
       <Switch>
         <ProtectedRoute path="/oi/home" component={Home} />
+        <ProtectedRoute path="/oi/csoportok" component={Csoportok} />
       </Switch>
     </div>
   );
